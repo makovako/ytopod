5165029dae12c4da9cb234a8f58fba3d97bcc95d
Marek Valko <valko.marek@gmail.com>
2020-05-06 16:46:28 +0200

    Basic navigation config

---
+++ flask/ytopod/nav.py
@@ -0,0 +1,17 @@
+nav = [
+    {
+        "name": "Home",
+        "url": "/",
+        "active": False
+    },
+    {
+        "name": "All",
+        "url": "/all",
+        "active": False
+    },
+    {
+        "name": "Download",
+        "url": "/download",
+        "active": False
+    },
+]
