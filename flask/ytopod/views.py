6e7f914fc8943c579faa36efa2ebfb795634288a
Marek Valko <valko.marek@gmail.com>
2020-05-06 16:46:59 +0200

    Navigation processing and sample video data

--- flask/ytopod/views.py
+++ flask/ytopod/views.py
@@ -1,5 +1,40 @@
 from ytopod import app
+from flask import render_template
+from .nav import nav
+
+sample_videos = [
+    {
+        "id": 1,
+        "name": "lala"
+    },
+    {
+        "id": 2,
+        "name":"lolo"
+    }
+]
+
+@app.before_request
+def clear_nav():
+    for link in nav:
+        link["active"] = False
 
 @app.route("/")
 def index():
-    return "Hello Wotld"
+    for link in nav:
+        if link["name"] == "Home":
+            link["active"] = True
+    return render_template("index.html", title="Home - ytopod", nav=nav)
+
+@app.route("/download")
+def download():
+    for link in nav:
+        if link["name"] == "Download":
+            link["active"] = True
+    return render_template("download.html", title="Download - ytopod", nav=nav)
+
+@app.route("/all")
+def all():
+    for link in nav:
+        if link["name"] == "All":
+            link["active"] = True
+    return render_template("all.html", title="All - ytopod", nav=nav, videos=sample_videos)
