1f5bbdaac817f09cc86d481a3aa7ed572c934b74
Marek Valko <valko.marek@gmail.com>
2020-05-09 13:17:29 +0200

    Added Login forms

--- flask/ytopod/models.py
+++ flask/ytopod/models.py
@@ -1,5 +1,26 @@
 from . import db
 from sqlalchemy.sql import func
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    modified = db.Column(db.DateTime, nullable=False, server_default=func.now(), server_onupdate=func.now())
+    def __init__(self, username):
+        self.username = username
+        
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
 
 class Video(db.Model):
     __tablename__ = "videos"
