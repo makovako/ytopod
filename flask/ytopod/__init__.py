0365ce3653f1f35eabd19e8aee5273a8d741bfdd
Marek Valko <valko.marek@gmail.com>
2020-05-09 13:16:38 +0200

    Added setup for login

--- flask/ytopod/__init__.py
+++ flask/ytopod/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from .utils import create_download_directory
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, static_url_path='', instance_relative_config=False)
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     create_download_directory()
 
     with app.app_context():
         from . import views
+        from . import auth
         db.create_all()
         return app
