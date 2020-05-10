2c701db63f292d5dd97dd7138974fe741a695710
Marek Valko <valko.marek@gmail.com>
2020-05-10 15:53:35 +0200

    Added socket io setup

--- flask/ytopod/__init__.py
+++ flask/ytopod/__init__.py
@@ -4,10 +4,12 @@ from flask_login import LoginManager
 from .utils import create_download_directory
 from flask_httpauth import HTTPBasicAuth
 from datetime import timedelta
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 http_basic_auth = HTTPBasicAuth()
+socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__, static_url_path='', instance_relative_config=False)
@@ -15,6 +17,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    socketio.init_app(app)
 
     create_download_directory()
 
